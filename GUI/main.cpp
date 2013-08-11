daf8f307ce3975380928beef8596a7321e6352fd
Vann Teves <vcteves@ucdavis.edu>
2013-08-11 15:52:28 -0700

    Added README. Updated main

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTimer *timer = new QTimer();
     MainWidget w;
+    timer->connect(timer, SIGNAL(timeout()), &w, SLOT(requestData()));
+    timer->start(378);
     w.show();
 
     return a.exec();
